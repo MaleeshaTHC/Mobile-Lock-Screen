33a6a86fefc87b067fa19b37b5a706f37dffe688
MaleeshaTHC <96755@fhss.sjp.ac.lk>
2021-10-04 20:09:31 +0530

    create main screen and button component for make number pad

---
+++ app/Numbers.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, TouchableHighlight, Text} from 'react-native';
+export default class Numbers extends Component {
+  render() {
+    let no = [
+      {id: 1},
+      {id: 2},
+      {id: 3},
+      {id: 4},
+      {id: 5},
+      {id: 6},
+      {id: 7},
+      {id: 8},
+      {id: 9},
+      {id: 0},
+    ];
+    return (
+      <View style={styles.childTwo}>
+        <View style={styles.container}>
+          {no.map(no => {
+            return (
+              <TouchableHighlight
+                activeOpacity={0.5}
+                underlayColor="#b3d1e3"
+                onPress={() => this._onPressNumber(no.id)}
+                style={styles.round}
+                key={no.id}>
+                <Text style={styles.numberText}>{no.id}</Text>
+              </TouchableHighlight>
+            );
+          })}
+        </View>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    width: 325,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+  },
+  round: {
+    width: 90,
+    height: 90,
+    borderRadius: 75,
+    backgroundColor: '#e6f5fa',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 8,
+  },
+  text: {
+    fontFamily: 'Roboto',
+    fontSize: 30,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: '#5095bf',
+  },
+});
