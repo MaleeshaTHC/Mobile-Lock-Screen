a70c78a854015a296b8b5a05d3a125f7971c2af5
MaleeshaTHC <96755@fhss.sjp.ac.lk>
2021-10-08 18:16:52 +0530

    complete main screen

--- app/main.js
+++ app/main.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import {TouchableHighlight, View, Text, StyleSheet} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       pincode: ['', '', '', ''],
+      main_phrase: 'Enter your PIN code',
     };
   }
 
@@ -52,8 +53,38 @@ export default class Main extends Component {
 
     return (
       <View style={styles.main}>
-        <View style={styles.upper} />
-        <View style={styles.lower} />
+        <View style={styles.upper}>
+          <Text style={styles.text_style}>Enter your PIN code</Text>
+          <View style={styles.background_container}>
+            {this.state.pincode.map(p => {
+              let style = p !== '' ? styles.pin_used : styles.pin_unused;
+              return <View style={style} />;
+            })}
+          </View>
+        </View>
+        <View style={styles.lower}>
+          <View style={styles.numbers_container}>
+            {number.map(number => {
+              return (
+                <TouchableHighlight
+                  activeOpacity={0.5}
+                  underlayColor="#b3d1e3"
+                  style={styles.circle}
+                  key={number.index}
+                  onPress={() => this.Press_Number(number.index)}>
+                  <Text style={styles.number}>{number.index}</Text>
+                </TouchableHighlight>
+              );
+            })}
+          </View>
+          <TouchableHighlight
+            activeOpacity={0.5}
+            underlayColor="#b3d1e3"
+            style={{left: 110, bottom: 80}}
+            onPress={() => this.Press_Erase()}>
+            <Icon name="backspace" size={50} color="#e6f5fa" />
+          </TouchableHighlight>
+        </View>
       </View>
     );
   }
@@ -72,5 +103,58 @@ const styles = StyleSheet.create({
     marginTop: 50,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text_style: {
+    fontFamily: 'Roboto',
+    fontSize: 22,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: '#5095bf',
+  },
+  pin_unused: {
+    width: 13,
+    height: 13,
+    borderRadius: 13,
+    borderWidth: 1,
+    borderColor: '#5095bf',
+  },
+  pin_used: {
+    width: 13,
+    height: 13,
+    borderRadius: 13,
+    borderWidth: 1,
+    backgroundColor: '#5095bf',
+    borderColor: '#5095bf',
+  },
+  background_container: {
+    marginTop: 90,
+    marginLeft: 150,
+    marginRight: 150,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  numbers_container: {
+    width: 325,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  circle: {
+    width: 90,
+    height: 90,
+    borderRadius: 90,
+    margin: 8,
+    backgroundColor: '#e6f5fa',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  number: {
+    fontFamily: 'Roboto',
+    fontSize: 30,
+    color: '#5095bf',
+    textAlign: 'center',
+    textAlignVertical: 'center',
   },
 });
